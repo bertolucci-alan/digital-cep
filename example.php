0ce91dbe95ba04eb902bf48af70738be73f8f582
bertolucci-alan <beterrababir23@gmail.com>
2021-07-28 15:24:38 -0300

    cidade de Garça/SP

--- example.php
+++ example.php
@@ -6,6 +6,6 @@ use \User\DigitalCep\Search;
 
 $search = new Search;
 
-$result = $search->getAddressFromZipCode('17400000');
+$result = $search->getAddressFromZipCode('17406514');
 
 print_r($result);
